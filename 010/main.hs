28e8a83ce8719cfc54f3a2d9a9c2543ac6b73d20
Julian Kalema Lukwata <julianlukwata@posteo.de>
2023-10-03 16:05:51 +0200

    add day 10 part 2

--- 010/main.hs
+++ 010/main.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import Data.Text (splitOn)
-import Data.Tree (flatten)
+import Data.Foldable (toList)
+import Data.List
+import Data.List.Split (chunksOf)
+import Data.Sequence (fromList, mapWithIndex)
 import System.Environment
 
 -- import Data.Text
@@ -11,10 +13,17 @@ main = do
   fileIo <- readFile "input.txt"
   let dataLines = foldMap lineToInstruction (lines fileIo)
   let register = scanl (+) 1 dataLines
-  print register
   print (sum (map (\x -> (register !! (x - 1)) * x) [20, 60, 100, 140, 180, 220]))
+  putStrLn (handleRegister register)
 
 lineToInstruction :: String -> [Int]
 lineToInstruction "noop" = [0]
 lineToInstruction ('a' : 'd' : 'd' : 'x' : ' ' : x) = [0, read x]
 lineToInstruction _ = []
+
+handleRegister :: [Int] -> String
+handleRegister register =
+  let chars = toList (mapWithIndex getCRT (fromList register))
+   in intercalate "\n" (chunksOf 40 chars)
+
+getCRT index v = if mod index 40 >= v - 1 && mod index 40 <= v + 1 then '#' else '.'
